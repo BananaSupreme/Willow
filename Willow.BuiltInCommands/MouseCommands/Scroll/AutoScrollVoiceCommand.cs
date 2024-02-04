633284a490b2810b67cb043a8d421729a221c0ab
BananaSupreme <Supremus>
2024-02-04 01:05:52 +0100

    .

--- Willow.BuiltInCommands/MouseCommands/Scroll/AutoScrollVoiceCommand.cs
+++ Willow.BuiltInCommands/MouseCommands/Scroll/AutoScrollVoiceCommand.cs
@@ -6,7 +6,6 @@ using Willow.Core.Environment.Abstractions;
 using Willow.Core.Environment.Models;
 using Willow.Core.Settings.Abstractions;
 using Willow.DeviceAutomation.InputDevices.Abstractions;
-using Willow.Helpers.Locking;
 using Willow.Speech.ScriptingInterface.Abstractions;
 using Willow.Speech.ScriptingInterface.Attributes;
 using Willow.Speech.ScriptingInterface.Models;
@@ -17,8 +16,7 @@ internal sealed class AutoScrollVoiceCommand : IVoiceCommand
 {
     public const string AutoScrollingTagString = "<command>_auto_scroll";
     private static readonly Tag _autoScrollingTag = new(AutoScrollingTagString);
-    private static CancellationTokenSource? _cts;
-    private static readonly DisposableLock _lock = new();
+    private bool _running;
     private readonly IEnvironmentStateProvider _environmentStateProvider;
     private readonly IInputSimulator _inputSimulator;
     private readonly ISettings<ScrollSettings> _settings;
@@ -34,7 +32,7 @@ internal sealed class AutoScrollVoiceCommand : IVoiceCommand
 
     public string InvocationPhrase => "scroll auto ?[[start|stop]:action]:__ ?[[up|down]:direction]:_";
 
-    public async Task ExecuteAsync(VoiceCommandContext context)
+    public Task ExecuteAsync(VoiceCommandContext context)
     {
         var action = context.Parameters.GetValueOrDefault("action")?.GetString() ?? "start";
         var direction = context.Parameters.GetValueOrDefault("direction")?.GetString() ?? "down";
@@ -46,48 +44,36 @@ internal sealed class AutoScrollVoiceCommand : IVoiceCommand
                 break;
 
             case "stop":
-                await Stop(_environmentStateProvider);
+                Stop(_environmentStateProvider);
                 break;
         }
+
+        return Task.CompletedTask;
     }
 
-    private static async Task Start(string direction,
-                                    IInputSimulator inputSimulator,
-                                    IEnvironmentStateProvider environmentStateProvider,
-                                    ISettings<ScrollSettings> settings)
+    private async Task Start(string direction,
+                             IInputSimulator inputSimulator,
+                             IEnvironmentStateProvider environmentStateProvider,
+                             ISettings<ScrollSettings> settings)
     {
-        if (_cts is not null || !_lock.CanEnter)
+        environmentStateProvider.ActivateTag(_autoScrollingTag);
+        if (_running)
         {
             return;
         }
 
-        using var unlocker = await _lock.LockAsync();
-
-        _cts = new CancellationTokenSource();
-
-        try
+        _running = true;
+        while (_running)
         {
-            environmentStateProvider.ActivateTag(_autoScrollingTag);
-            while (!_cts.IsCancellationRequested)
-            {
-                inputSimulator.Scroll(GetFromDirection(direction));
-                await Task.Delay(settings.CurrentValue.Speed);
-            }
-        }
-        finally
-        {
-            _cts.Dispose();
-            _cts = null;
+            inputSimulator.Scroll(GetFromDirection(direction));
+            await Task.Delay(settings.CurrentValue.Speed);
         }
     }
 
-    private static async Task Stop(IEnvironmentStateProvider environmentStateProvider)
+    private void Stop(IEnvironmentStateProvider environmentStateProvider)
     {
-        if (_cts is not null)
-        {
-            environmentStateProvider.DeactivateTag(_autoScrollingTag);
-            await _cts.CancelAsync();
-        }
+        environmentStateProvider.DeactivateTag(_autoScrollingTag);
+        _running = false;
     }
 
     [Tag(AutoScrollingTagString)]
